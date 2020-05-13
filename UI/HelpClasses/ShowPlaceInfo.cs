140bc4c4624929e30b83a72df99de51c6baa5d16
Sudoku <sanyatv@gmail.com>
2020-05-13 09:17:01 +0300

    коментування showplace

---
+++ UI/HelpClasses/ShowPlaceInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI.HelpClasses
+{
+    public class ShowPlaceInfo
+    {
+        public string IMG { get; set; }
+        public string NAME { get; set; }
+        public string CITY { get; set; }
+        public string COUNTRY { get; set; }
+    }
+}
